31393870790e81d1dbc2e2e21e672f64c45f6f67
Dharaaaaaa <85234186+Dharaaaaaa@users.noreply.github.com>
2023-07-25 18:04:38 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,34 @@
+// Create web server
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+// Create express app
+const app = express()
+
+// Middleware
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+// Route
+app.get('/posts/:id/comments', (req, res) => {
+  res.send([
+    {
+      id: 1,
+      content: 'Comment #1',
+      postId: req.params.id
+    },
+    {
+      id: 2,
+      content: 'Comment #2',
+      postId: req.params.id
+    }
+  ])
+})
+
+// Start server
+app.listen(4001, () => {
+  console.log('Listening on 4001')
+})
